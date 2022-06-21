6967f049306cdff243d81f4e195ee8da81b5acb1
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2022-06-21 18:39:07 +0500

    clean code, add server express script

--- app.js
+++ app.js
@@ -1,42 +1,26 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = process.env.PORT || 8080;
+const servicesNavigateLeftButton = document.querySelector('.body-container-third-header-other-navigate-button.left');
+const servicesNavigateRightButton = document.querySelector('.body-container-third-header-other-navigate-button.right');
+const servicesCards = document.querySelector('.body-container-third-cards');
+const upButton = document.querySelector('.body-form-header-up-button');
 
-// const servicesNavigateLeftButton = document.querySelector('.body-container-third-header-other-navigate-button.left');
-// const servicesNavigateRightButton = document.querySelector('.body-container-third-header-other-navigate-button.right');
-// const servicesCards = document.querySelector('.body-container-third-cards');
-// const upButton = document.querySelector('.body-form-header-up-button');
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));    
+servicesNavigateLeftButton.addEventListener('click', () => {
+    servicesCards.scrollBy({
+        left: -400,
+        behavior: "smooth"
+    });    
 });
 
-app.listen(port, () => {
-    console.log(`listening on port ${port}!`)
-    console.log(__dirname)
+servicesNavigateRightButton.addEventListener('click', () => {    
+    servicesCards.scrollBy({
+        left: 400,
+        behavior: "smooth"
+    });    
 });
 
-app.use(express.static(__dirname + '/'));
-
-// servicesNavigateLeftButton.addEventListener('click', () => {
-//     servicesCards.scrollBy({
-//         left: -400,
-//         behavior: "smooth"
-//     });    
-// });
-
-// servicesNavigateRightButton.addEventListener('click', () => {    
-//     servicesCards.scrollBy({
-//         left: 400,
-//         behavior: "smooth"
-//     });    
-// });
-
-// upButton.addEventListener('click', () => {
-//     window.scrollTo({
-//         top: '100%',
-//         behavior: "smooth"
-//     })
-// })
+upButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: '100%',
+        behavior: "smooth"
+    })
+})
 
--- serv.js
+++ serv.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));    
 });
 
 app.listen(port, () => {
-    console.log('sdgdfdfdfg')
-    // console.log(`listening on port ${port}!`);
-})
+    console.log(`listening on port ${port}!`)
+    console.log(__dirname)
+});
 
+app.use(express.static(__dirname + '/'));
